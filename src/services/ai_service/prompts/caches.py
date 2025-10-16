35fdcbf42e22896bbeb691e1c2bca5e6553f63b5
soheil <soheil@praxos.ai>
2025-10-15 23:10:24 -0300

    new cache

--- src/services/ai_service/prompts/caches.py
+++ src/services/ai_service/prompts/caches.py
@@ -4,7 +4,7 @@ from src.config.settings import settings
 from src.utils.logging import setup_logger
 logger = setup_logger(__name__)
 ### IMPORTANT NOTE. FOR PLANNER TO WORK, YOU REALLY SHOULD USE A CACHE, OR OTHERWISE, RECODE IT TO NOT USE A CACHE. 
-PLANNING_CACHE_NAME = 'cachedContents/e5x2m4fq7q8b7lh4bw86efgisu2l0h7hj9emutiy'
+PLANNING_CACHE_NAME = 'cachedContents/yzbi7vycknhueqfpr73ts8l71irycb127617rq2w'
 
 async def update_cache_ttl():
     """Updates the cache TTL in a fire-and-forget manner."""

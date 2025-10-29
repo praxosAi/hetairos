7c04125c845f768fd38363983d9f77b2d71ed364
masoud <masoudkermanipoor@gmail.com>
2025-10-29 17:55:19 -0400

    do not send intermediate message for the browser tool

--- src/services/ai_service/prompts/caches.py
+++ src/services/ai_service/prompts/caches.py
@@ -4,7 +4,7 @@ from src.config.settings import settings
 from src.utils.logging import setup_logger
 logger = setup_logger(__name__)
 ### IMPORTANT NOTE. FOR PLANNER TO WORK, YOU REALLY SHOULD USE A CACHE, OR OTHERWISE, RECODE IT TO NOT USE A CACHE. 
-PLANNING_CACHE_NAME = 'cachedContents/xz4u21iml5b0bz51t8t4acrpzddfppwfbgpj6z9a'
+PLANNING_CACHE_NAME = 'cachedContents/o03r18oksazievf15kin110vmg5gg4gfdjldvvzt'
 async def update_cache_ttl():
     """Updates the cache TTL in a fire-and-forget manner."""
     try:

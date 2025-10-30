e1c708482841f9f30b85d5ebcc444401c40ef005
masoud <masoudkermanipoor@gmail.com>
2025-10-30 17:25:10 -0400

    currectly caluculate next cron job time

--- src/utils/timezone_utils.py
+++ src/utils/timezone_utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone, timedelta
 from typing import Union
 from zoneinfo import ZoneInfo # Use this for Python 3.9+
-def nyc_to_utc(nyc_dt: datetime, timezone_name: str = "America/New_York") -> datetime:
+def nyc_to_utc(nyc_dt: datetime, timezone: str|ZoneInfo = "America/New_York") -> datetime:
     """
     Converts a naive datetime object from NYC time to UTC.
 
@@ -10,17 +10,28 @@ def nyc_to_utc(nyc_dt: datetime, timezone_name: str = "America/New_York") -> dat
     timezone database.
     """
     # Define the New York timezone using the IANA database
-    nyc_tz = ZoneInfo(timezone_name)
+    if isinstance(timezone, str):
+        zoneinfo = to_ZoneInfo(timezone)
+    else:
+        zoneinfo = timezone
     
     # Localize the naive datetime by applying the NYC timezone.
     # This step correctly determines whether the datetime falls in EST or EDT.
-    aware_nyc_dt = nyc_dt.replace(tzinfo=nyc_tz)
+    aware_nyc_dt = nyc_dt.replace(tzinfo=zoneinfo)
     
     # Convert the timezone-aware NYC datetime to UTC
     utc_dt = aware_nyc_dt.astimezone(timezone.utc)
     
     return utc_dt
 
+def to_ZoneInfo(timezone_name: str) -> ZoneInfo:
+    """
+    Converts a timezone name string to a ZoneInfo object.
+    """
+    try:
+        return ZoneInfo(timezone_name)
+    except Exception as e:
+        return ZoneInfo("America/New_York")  # Fallback to NYC
 
 def to_rfc3339(dt: Union[datetime, str]) -> str:
     if isinstance(dt, str):
